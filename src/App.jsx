e6b364e1d487aed1ee0c146276f1c6104bffab8c
AnilKumar3494 <karapaanilkumar005@gmail.com>
2025-02-14 00:42:50 -0500

    bmv init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import "./App.css";
-import Happyemojirain from "./components/HappyEmojiRain";
 import Valentine from "./components/Valentine";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Valentine />
--- src/components/Valentine.jsx
+++ src/components/Valentine.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import { FaArrowLeft } from "react-icons/fa";
 import Happyemojirain from "./HappyEmojiRain";
 import Sademojiesrain from "./SadEmojiRain";
@@ -11,8 +10,8 @@ const Valentine = () => {
   const [yesBtnIndex, setYesBtnIndex] = useState(0);
   const [noBtnIndex, setNoBtnIndex] = useState(0);
   const [clickedYes, setClickedYes] = useState(null);
-  //emoji rain
   const [showHappyEmojirain, setHappyShowEmojirain] = useState(null);
+  const [yesBtnSize, setYesBtnSize] = useState(1); // Initial size multiplier
 
   const yesBtnContentArray = [
     "Yes",
@@ -37,29 +36,32 @@ const Valentine = () => {
       } else {
         setHappyShowEmojirain(null);
       }
-
       return newIndex;
     });
     setNoBtnIndex(0);
+    setYesBtnSize(1); // Reset size when clicking Yes
   }
 
   const changeNoBtnContent = () => {
     setClickedYes(false);
     setNoBtnIndex((prevIndex) => {
       const newIndex = (prevIndex + 1) % noBtnContentArray.length;
-      newIndex === noBtnContentArray.length - 1 && setHappyShowEmojirain(null);
+      if (newIndex === noBtnContentArray.length - 1) {
+        setHappyShowEmojirain(null);
+      }
       return newIndex;
     });
     setYesBtnIndex(0);
     setHappyShowEmojirain(false);
+    setYesBtnSize((prevSize) => prevSize + 0.3); // Increase size of Yes button
   };
 
   return (
     <main>
       <section className="valentine_section">
         <div className="test">
-          <div className="question ">
-            {/* emoji rain control - frontend */}
+          <div className="question">
+            {/* emoji rain control */}
             {showHappyEmojirain === null ? (
               ""
             ) : showHappyEmojirain ? (
@@ -70,14 +72,22 @@ const Valentine = () => {
 
             <h2>Will you be my Valentine?</h2>
 
-            {/* YES BUTTON */}
             <div className="buttons">
+              {/* YES BUTTON */}
               {yesBtnIndex === yesBtnContentArray.length - 1 ? (
-                <button className="btn yes" onClick={changeYesBtnContent}>
+                <button
+                  className="btn yes"
+                  onClick={changeYesBtnContent}
+                  style={{ transform: `scale(${yesBtnSize})` }} // Dynamic size
+                >
                   ❤️💓
                 </button>
               ) : (
-                <button className="btn yes" onClick={changeYesBtnContent}>
+                <button
+                  className="btn yes"
+                  onClick={changeYesBtnContent}
+                  style={{ transform: `scale(${yesBtnSize})` }} // Dynamic size
+                >
                   Yes
                 </button>
               )}
